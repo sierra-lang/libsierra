09d40437b4b4c61f264df76e4c6ac74d4d2c51fa
Roland Leißa <leissa@cs.uni-saarland.de>
2012-05-14 19:49:18 -0700

    mandelbrot example working

---
+++ examples/mandelbrot.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <stdio.h>
+#include "../sierra.h"
+#include "../timing.h"
+
+using namespace sierra;
+
+#define L 8
+//#define sierra_program_index (size_t varying(L)){0, 1, 2, 3}
+#define sierra_program_index (size_t varying(L)){0, 1, 2, 3, 4, 5, 6, 7}
+//#define sierra_program_index (size_t varying(L)){0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+
+static inline int varying(L) mandel(float varying(L) c_re, float varying(L) c_im, int uniform count) {
+    float varying(L) z_re = c_re, z_im = c_im;
+    int varying(L) i = 0;
+
+    while ((i < count) & (z_re * z_re + z_im * z_im < 4.f)) {
+        float varying(L) new_re = z_re*z_re - z_im*z_im;
+        float varying(L) new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+
+        i = i + 1;
+    }
+
+    return i;
+}
+
+static void mandelbrot(float x0, float y0, 
+                       float x1, float y1,
+                       int width, int height, 
+                       int maxIterations, int output[])
+{
+    float dx = (x1 - x0) / width;
+    float dy = (y1 - y0) / height;
+
+    for (int j = 0; j < height; ++j) {
+        for (int ii = 0; ii < width; ii += L) {
+            int varying(L) i = ii + sierra_program_index;
+            float varying(L) x = x0 + i * dx;
+            float varying(L) y = y0 + j * dy;
+
+            int varying(L) index = j * width + i;
+            int varying(L) val = mandel(x, y, maxIterations);
+            output[extract(index, 0)] = extract(val, 0);
+            output[extract(index, 1)] = extract(val, 1);
+            output[extract(index, 2)] = extract(val, 2);
+            output[extract(index, 3)] = extract(val, 3);
+        }
+    }
+}
+
+
+/* Write a PPM image file with the image of the Mandelbrot set */
+static void
+writePPM(int *buf, int width, int height, const char *fn) {
+    FILE *fp = fopen(fn, "wb");
+    fprintf(fp, "P6\n");
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "255\n");
+    for (int i = 0; i < width*height; ++i) {
+        // Map the iteration count to colors by just alternating between
+        // two greys.
+        char c = (buf[i] & 0x1) ? 240 : 20;
+        for (int j = 0; j < 3; ++j)
+            fputc(c, fp);
+    }
+    fclose(fp);
+    printf("Wrote image file %s\n", fn);
+}
+
+int main() {
+    unsigned int width = 768;
+    unsigned int height = 512;
+    float x0 = -2;
+    float x1 = 1;
+    float y0 = -1;
+    float y1 = 1;
+
+    int maxIterations = 256;
+    int *buf = new int[width*height];
+
+    double minISPC = 1e30;
+    reset_and_start_timer();
+    mandelbrot(x0, y0, x1, y1, width, height, maxIterations, buf);
+    double dt = get_elapsed_mcycles();
+    minISPC = std::min(minISPC, dt);
+
+    printf("[mandelbrot ispc]:\t\t[%.3f] million cycles\n", minISPC);
+
+    writePPM(buf, width, height, "mandelbrot-sierra.ppm");
+
+    return 0;
+}
