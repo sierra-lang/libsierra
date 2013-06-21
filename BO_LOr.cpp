94cac0dc0a810575c08b896e9f4c3ef8bf7f7bb3
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-06-21 15:49:12 +0200

    edit BO_LOr

--- BO_LOr.cpp
+++ BO_LOr.cpp
@@ -1,17 +1,29 @@
 #include "sierra.h"
-#include <iostream>
+//#include <iostream>
 
 int main()
 {
     bool varying(4) a = {true, true, false, true};
     bool varying(4) b = {false, false, false, true};
 
-    int x = 0;
+    int varying(4) x = sierra::seq<4>();
+
+    /*
+    for ( int i = 0; i < 4; i++ )
+        std::cout << sierra::extract<int,4>( x, i ) << "\t";
+    std::cout << std::endl;
+    */
 
     if ( a || b )
         x++;
+    else
+        x--;
 
-    std::cout << x << std::endl;
+    /*
+    for ( int i = 0; i < 4; i++ )
+        std::cout << sierra::extract<int,4>( x, i ) << "\t";
+    std::cout << std::endl;
+    */
 
     return 0;
 }
