3faa4c4a1f8fbcaabbcd525257b07bbfad47db6f
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-07-04 17:56:33 +0200

    added test case for sierra logical not

---
+++ simple_logical_not.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+    bool __attribute__((sierra_vector(4))) a = {true, true, false, true};
+    bool __attribute__((sierra_vector(4))) b = {false, false, false, true};
+
+    int __attribute__((sierra_vector(4))) x = 0;
+
+    if ( ! ( a || b ) )
+        x++;
+
+    return ((int*) &x)[0];
+}
