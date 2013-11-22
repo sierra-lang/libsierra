78eb2a36ac2be1568f37f24a478e1cfe06d2b366
Roland Leißa <leissa@cs.uni-saarland.de>
2013-11-22 10:31:30 +0100

    bug fix in add

--- vec3.h
+++ vec3.h
@@ -13,18 +13,18 @@ template<int L>
 spmd(L) 
 vec3 varying(L) add(vec3 varying(L) v, vec3 varying(L) w) {
     vec3 varying(L) res;
-    res.x = v.x + v.x;
-    res.y = v.y + v.y;
-    res.z = v.z + v.z;
+    res.x = v.x + w.x;
+    res.y = v.y + w.y;
+    res.z = v.z + w.z;
     return res;
 }
 
 spmd(4) 
 vec3 varying(4) add4(vec3 varying(4) v, vec3 varying(4) w) {
     vec3 varying(4) res;
-    res.x = v.x + v.x;
-    res.y = v.y + v.y;
-    res.z = v.z + v.z;
+    res.x = v.x + w.x;
+    res.y = v.y + w.y;
+    res.z = v.z + w.z;
     return res;
 }
 
--- vec3_test.cpp
+++ vec3_test.cpp
@@ -8,7 +8,7 @@ using namespace sierra;
 int main() {
     vec3 varying(4) a;
     vec3 varying(4) b;
-    a.x = (float varying(4)) {0, 1, 2, 3};
+    a.x = (float varying(4)) {1, 1, 2, 3};
     a.y = (float varying(4)) {0, 1, 2, 3};
     a.z = (float varying(4)) {0, 1, 2, 3};
     b.x = (float varying(4)) {0, 1, 2, 3};
@@ -18,7 +18,7 @@ int main() {
     c.x = c.y = c.z = (float varying(4)){0, 0, 0, 0};
 
     if ((bool varying(4)){false, false, true, true})
-        c = add4(a, b);
+        c = add<4>(a, b);
 
     std::cout << c.x << std::endl;
 }
