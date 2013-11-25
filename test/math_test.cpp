42af5c692e94f796dbc51de8ff27ed761fc1367b
Roland Leißa <leissa@cs.uni-saarland.de>
2013-11-25 13:19:47 +0100

    tesint more math functions

--- test/math_test.cpp
+++ test/math_test.cpp
@@ -8,7 +8,14 @@
 using namespace sierra;
 
 int main() {
-    float varying(4) f = {0, 2, 3, 4};
-    float varying(4) e = exp(f);
-    std::cout << e << std::endl;
+    float varying(4) a = {0, 1,  2,  3};
+    float varying(4) b = {4, 5,  6,  7};
+    float varying(4) c = {8, 9, 10, 11};
+    std::cout << exp(a) << std::endl;
+    std::cout << log(a) << std::endl;
+    std::cout << log2(a) << std::endl;
+    std::cout << log10(a) << std::endl;
+    std::cout << sin(a) << std::endl;
+    std::cout << cos(a) << std::endl;
+    std::cout << fma(a, b, c) << std::endl;
 }
