4c838d4e90c861c4e2cfaa07d3175a0f85427ab9
Roland Leißa <leissa@cs.uni-saarland.de>
2013-11-27 22:12:27 +0100

    it works :)

--- benchmark/aobench/ao_sierra.cpp
+++ benchmark/aobench/ao_sierra.cpp
@@ -90,6 +90,7 @@ void ray_plane_intersect(Isect varying(L)& isect, Ray varying(L)& ray, Plane uni
             //isect.p = ray.org + ray.dir * t;
             mul(isect.p, ray.dir, t);
             add_assign(isect.p, ray.org);
+            copy(isect.n, plane_n);
         }
     }
 }
@@ -205,13 +206,13 @@ float varying(L) ambient_occlusion(Isect varying(L)& isect, Plane uniform& plane
     return occlusion;
 }
 
+#define NSUBSAMPLES 16
 
 /* Compute the image for the scanlines from [y0,y1), for an overall image
    of width w and height h.
  */
 static void ao_scanlines(int uniform y0, int uniform y1, int uniform w, 
-                         int uniform h,  int uniform nsubsamples, 
-                         float uniform image[]) {
+                         int uniform h,  float uniform image[]) {
     static Plane uniform plane = {{ 0.0f, -0.5f, 0.0f }, { 0.f, 1.f, 0.f }};
     static Sphere uniform spheres[3] = {{{ -2.0f, 0.0f, -3.5f }, 0.5f },
                                         {{ -0.5f, 0.0f, -3.0f }, 0.5f },
@@ -219,69 +220,67 @@ static void ao_scanlines(int uniform y0, int uniform y1, int uniform w,
     RNGState varying(L) rngstate;
     spmd_mode(L)
         seed_rng(rngstate, seq<L>() + (y0 << (seq<L>() & 15)));
-    float invSamples = 1.f / nsubsamples;
+    float invSamples = 1.f / NSUBSAMPLES;
 
     //foreach_tiled(y = y0 ... y1, x = 0 ... w, 
                   //u = 0 ... nsubsamples, v = 0 ... nsubsamples)
     for (int y = y0; y < y1; ++y) {
-        for (int xx = 0; xx < w; xx += L) {
-            int varying(L) x = seq<L>() + xx;
-            spmd_mode(L) {
-            //for (int u = 0; u < nsubsamples; ++u)
-                //for (int v = 0; v < nsubsamples; ++v)
-                int u = 0;
-                int v = 0;
+        for (int x = 0; x < w; ++x) {
+            for (int u = 0; u < NSUBSAMPLES; ++u) {
+                for (int vv = 0; vv < NSUBSAMPLES; vv += L) {
+                    int varying(L) v = vv + seq<L>();
                     float du = (float)u * invSamples;
-                    float dv = (float)v * invSamples;
+                    float varying(L) dv = (float varying(L))v * invSamples;
 
                     // Figure out x,y pixel in NDC
-                    float varying(L) px =  (x + du - (w / 2.0f)) / (w / 2.0f);
-                    float            py = -(y + dv - (h / 2.0f)) / (h / 2.0f);
-                    float varying(L) ret = 0.f;
+                    float            px =  (x + du - (w / 2.0f)) / (w / 2.0f);
+                    float varying(L) py = -(y + dv - (h / 2.0f)) / (h / 2.0f);
+                    //float varying(L) ret = 0.f;
                     Ray varying(L) ray;
                     Isect varying(L) isect;
 
-                    create(ray.org, 0.f, 0.f, 0.f);
+                    spmd_mode(L)
+                        create(ray.org, 0.f, 0.f, 0.f);
 
                     // Poor man's perspective projection
                     ray.dir.x = px;
                     ray.dir.y = py;
                     ray.dir.z = -1.0f;
-                    normalize(ray.dir);
+                    spmd_mode(L)
+                        normalize(ray.dir);
 
                     isect.t   = 1.0e+17f;
                     isect.hit = 0;
 
-                    for (int uniform snum = 0; snum < 3; ++snum)
-                        ray_sphere_intersect(isect, ray, spheres[snum]);
-                    ray_plane_intersect(isect, ray, plane);
+                    for (int uniform snum = 0; snum < 3; ++snum) {
+                        spmd_mode(L)
+                            ray_sphere_intersect(isect, ray, spheres[snum]);
+                    }
+                    spmd_mode(L)
+                        ray_plane_intersect(isect, ray, plane);
 
                     if (isect.hit) {
-                        ret = ambient_occlusion(isect, plane, spheres, rngstate);
+                        float varying(L) ret = ambient_occlusion(isect, plane, spheres, rngstate);
                         ret *= invSamples * invSamples;
 
-                        //int varying(L) offset = 3 * (y * w + x);
-                        //atomic_add_local(&image[offset], ret);
-                        //atomic_add_local(&image[offset+1], ret);
-                        //atomic_add_local(&image[offset+2], ret);
+                        float result = 0.f;
+                        for (int uniform i = 0; i < L; ++i)
+                            result += extract(ret, i);
 
-                        for (int uniform i = 0; i != LENGTH; ++i) {
-                            image[3 * (y * w + xx + i) + 0] = extract(ret, i);
-                            image[3 * (y * w + xx + i) + 1] = extract(ret, i);
-                            image[3 * (y * w + xx + i) + 2] = extract(ret, i);
-                        }
+
+                        image[3 * (y * w + x) + 0] += result;
+                        image[3 * (y * w + x) + 1] += result;
+                        image[3 * (y * w + x) + 2] += result;
                     }
-                //}
-            //}
+                }
             }
         }
     }
 }
 
-
 void ao_sierra(uniform int w, uniform int h, uniform int nsubsamples, 
                     uniform float image[]) {
-    ao_scanlines(0, h, w, h, nsubsamples, image);
+    ao_scanlines(0, h, w, h, image);
 }
 
 #if 0
