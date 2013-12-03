d1876130baf913ceb1719e0b077371dea9ff5700
Roland Leißa <leissa@cs.uni-saarland.de>
2013-12-03 11:32:04 +0100

    added dummy returns to bypass warnings

--- sierra/math.h
+++ sierra/math.h
@@ -8,23 +8,23 @@ namespace sierra {
 #define floatbits(i) *(float varying(L)*) (&i)
 #define   intbits(f) *(int   varying(L)*) (&f)
 
-static float varying(L) exp(float varying(L));
-static float varying(L) exp2(float varying(L));
-static float varying(L) log(float varying(L));
-static float varying(L) log2(float varying(L));
-static float varying(L) log10(float varying(L));
-static float varying(L) pow(float varying(L), float varying(L));
-static float varying(L) powi(float varying(L), int varying(L));
-static float varying(L) sqrt(float varying(L));
-static float varying(L) sin(float varying(L));
-static float varying(L) cos(float varying(L));
-//static float varying(L) fma(float varying(L), float varying(L), float varying(L)); // fused mutpliy-add
-static float varying(L) fabs(float varying(L));
-static float varying(L) floor(float varying(L));
-static float varying(L) ceil(float varying(L));
-static float varying(L) trunc(float varying(L));
-static float varying(L) rint(float varying(L));
-static float varying(L) nearbyint(float varying(L));
+static float varying(L) exp(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) exp2(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) log(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) log2(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) log10(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) pow(float varying(L), float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) powi(float varying(L), int varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) sqrt(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) sin(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) cos(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) fma(float varying(L), float varying(L), float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) fabs(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) floor(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) ceil(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) trunc(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) rint(float varying(L)) { /* dummy */ return 0.f; }
+static float varying(L) nearbyint(float varying(L)) { /* dummy */ return 0.f; }
 
 spmd(L)
 inline float varying(L) fast_sin(float varying(L) x_full) {
