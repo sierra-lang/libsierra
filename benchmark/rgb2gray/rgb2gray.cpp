d3e79a7daf3348cb562f532e52a1b06a3d270333
Roland Leißa <leissa@cs.uni-saarland.de>
2014-07-14 22:06:08 +0200

    more work on reading input

--- benchmark/rgb2gray/rgb2gray.cpp
+++ benchmark/rgb2gray/rgb2gray.cpp
@@ -4,11 +4,14 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 
 #include "sierra/sierra.h"
 #include "sierra/timing.h"
 
 #define N 1048576
+
+#if 0
 
 alignas(16) float r[N+10000];
 alignas(16) float g[N+20000];
@@ -20,7 +23,72 @@ void serial_rgb2gray(float varying(L)* ra, float varying(L)* ga, float varying(L
         gray[i] = 0.3f * ra[i] + 0.59f * ga[i] + 0.11f * ba[i];
 }
 
+#endif
+
+//struct Image {
+    //int width;
+    //int height;
+//};
+
+//Image create(int width, int height) {
+    //Image result;
+    //result.r = new float[width*height];
+    //result.g = new float[width*height];
+    //result.b = new float[width*height];
+    //return result;
+//}
+
+
+int read_int(FILE* file) {
+    int result = 0;
+    while (true) {
+        int c = fgetc(file);
+        int digit = c - '0';
+        if (0 <= digit && digit <= 9) {
+            result *= 10;
+            result += digit;
+        } else {
+            ungetc(c, file);
+            break;
+        }
+    }
+
+    return result;
+}
+
 int main (int argc, char* argv[]) {
+    int width;
+    int height;
+    float* r = 0;
+    float* g = 0;
+    float* b = 0;
+
+    // read PPM
+    FILE* file = fopen("lena.ppm", "r");
+    if (!file)
+        return EXIT_FAILURE;
+
+    if (fgetc(file) == 'P' && fgetc(file) == '6' && fgetc(file) == '\n') {
+        width = read_int(file);
+        if (fgetc(file) == ' ') {
+            height = read_int(file);
+            if (fgetc(file) == '\n' && fgetc(file) == '2' && fgetc(file) == '5' && fgetc(file) == '5' && fgetc(file) == '\n') {
+                r = new[width*height];
+                g = new[width*height];
+                b = new[width*height];
+
+                while (true) {
+                    int ri = fgetc(file);
+                    if (ri !=
+                }
+                i = 0;
+                int ri = fgetc(file)
+            }
+        }
+    }
+    fclose(file);
+
+#if 0
     for(int i = 0; i < N; i++) {
         r[N] = random() % 256;
         g[N] = random() % 256;
@@ -32,4 +100,11 @@ int main (int argc, char* argv[]) {
     for(int i = 0; i < ITERATIONS; i++) { serial_rgb2gray(r, g, b, gray);}
         double dt = get_elapsed_seconds();
     std::cout<< "serial version: " << dt << " seconds" << std::endl;
+#endif
+
+    delete[] r;
+    delete[] g;
+    delete[] b;
+
+    return EXIT_SUCCESS;
 }
