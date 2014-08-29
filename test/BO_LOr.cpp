4d6ec966f508303719ea997237c6e1b92dd9b24e
Immanuel Haffner <haffner.immanuel@gmail.com>
2014-08-29 14:03:50 +0200

    avoid use of the sierra namespace (although the include "sierra.h" is working,     the namespace was not declared)

--- test/BO_LOr.cpp
+++ test/BO_LOr.cpp
@@ -6,7 +6,7 @@ int main()
     bool varying(4) a = {true, true, false, true};
     bool varying(4) b = {false, false, false, true};
 
-    int varying(4) x = sierra::seq<4>();
+    int varying(4) x = {0, 1, 2, 3};
 
     /*
     for ( int i = 0; i < 4; i++ )
