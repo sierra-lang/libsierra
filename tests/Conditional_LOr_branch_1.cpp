ec3c6ec48208875addfb7ad30adc18c9d87b794b
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-09-09 15:08:13 +0200

    added Conditional tests

---
+++ tests/Conditional_LOr_branch_1.cpp
@@ -0,0 +1,21 @@
+#include "sierra/defines.h"
+#include "test.h"
+
+int main()
+{
+	bool varying(4) a = {true, true, false, false};
+	bool varying(4) b = {true, false, true, false};
+	bool varying(4) c = {true, false, true, false};
+	bool varying(4) d = {false, false, true, true};
+
+	int varying(4) x = {0, 1, 2, 3};
+
+	if ( ( a ? b : c ) || d )	// < T, F, T, T >
+		++x;
+	else
+		--x;
+
+	return unify( x, 1, 0, 3, 4 );
+}
+
+// $RESULT: 15
