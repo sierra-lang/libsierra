4fa8aa83c7dfe62c711f84ee49c2072da62c606c
Immanuel Haffner <haffner.immanuel@gmail.com>
2014-09-10 23:23:38 +0200

    added two new tests for LOr + Conditional (of which one failes)

---
+++ tests/LOr_Conditional_branch_1.cpp
@@ -0,0 +1,21 @@
+#include "sierra/defines.h"
+#include "test.h"
+
+int main()
+{
+	bool varying(4) a = {true, true, false, false};
+	bool varying(4) b = {true, false, true, false};
+	bool varying(4) c = {false, true, false, true};
+	bool varying(4) d = {false, false, true, false};
+
+	int varying(4) x = {0, 1, 2, 3};
+
+	if ( d || ( a ? b : c ) )	// < T, F, T, T >
+		++x;
+	else
+		--x;
+
+	return unify( x, 1, 0, 3, 4 );
+}
+
+// $RESULT: 15
