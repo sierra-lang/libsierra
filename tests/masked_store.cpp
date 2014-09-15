217381758a7e27e8d744364d9dbb6495e80f9c60
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-09-15 13:23:59 +0200

    added test for masked store

---
+++ tests/masked_store.cpp
@@ -0,0 +1,22 @@
+#include "sierra/defines.h"
+#include "test.h"
+
+int varying(4) g = 0;
+
+spmd(4)
+void store( bool varying(4) &b )
+{
+  if ( b )
+    g = 1;
+}
+
+int main()
+{
+	bool varying(4) b = { true, true, false, false };
+  store( b );
+
+
+	return unify( g, 1, 1, 0, 0 );
+}
+
+// $RESULT: 15
