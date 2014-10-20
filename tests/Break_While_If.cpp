6cf5ecdd90e32d9d6a4665baa81f0090628adc50
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-10-20 12:19:05 +0200

    changed Break-For/While tests

--- tests/Break_While_If.cpp
+++ tests/Break_While_If.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int varying(4) n = 0;
+  int varying(4) n = 0;
 
-    int varying(4) i = { 0, 1, 2, 3 };
-    while ( bool varying(4) mask = true )
-    {
-      i += 4;
-			if ( i > 10 )
-				break;
-      ++n;
-    }
-    
-		return ( unify( i, 12, 13, 14, 11 ) << 4 ) | unify( n, 2, 2, 2, 1 );;
+  int varying(4) i = { 0, 1, 2, 3 };
+  while ( i < 10 )
+  {
+    if ( i > 2 )
+      break;
+    ++n;
+    i += 4;
+  }
+
+  return ( unify( i, 4, 5, 6, 3 ) << 4 ) | unify( n, 1, 1, 1, 0 );
 }
 
 // $RESULT: 255 = 0b1111 1111
