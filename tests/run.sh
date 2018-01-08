698b0fb1039a1c9df56806c20ba10a840a9e9139
Wiam Rachid <s9wirach@stud.uni-saarland.de>
2018-01-08 10:58:23 +0100

    added some simple tests

--- tests/run.sh
+++ tests/run.sh
@@ -2,8 +2,13 @@
 
 
 FILENAME="${1%.*}"
-LIBCLANG="-Xclang -load -Xclang $INSTALL_DIR/lib/libclang.so"
+#LIBCLANG="-Xclang -load -Xclang $INSTALL_DIR/lib/libclang.so"
 
 
 echo "------> emit ll file for $FILENAME"
-$CC -S -emit-llvm $LIBCLANG -fsierra -I.. $1 -o $FILENAME.ll
+#$CXX -S -emit-llvm $LIBCLANG -fsierra -I.. $1 -o $FILENAME.ll
+$CXX -S -emit-llvm -std=c++11 -fsierra -I.. $1 -o $FILENAME.ll
+
+
+echo "------> opt ll file for $FILENAME with mem2reg"
+$OPT -S -mem2reg ${FILENAME}.ll &> ${FILENAME}.opt.ll
